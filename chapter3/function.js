76a02c9a585e19162c5b65e21fb5b0f113795e58
Raihan Rafid <raihanrfd1@gmail.com>
2024-05-21 22:23:46 +0700

    init project

---
+++ chapter3/function.js
@@ -0,0 +1,36 @@
+//  Fungsi ini akan menjumlahkan nilai x dan y dan dikalikkan 2
+function perkalian(x, y) {
+  // Biar ga kena error NaN
+  if (typeof x !== "number") {
+    return "x harus number";
+  }
+
+  if (typeof y !== "number") {
+    return "y harus number";
+  }
+
+  return x * y;
+}
+
+//  Fungsi ini akan menjumlahkan nilai x dan y dan dikalikkan 2
+function jumlahDanKaliDua(x, y) {
+  let hasilJumlah = x + y;
+  let hasilAkhir = hasilJumlah * 2;
+
+  return hasilAkhir;
+}
+
+// Fungsi untuk pangkat 2
+function square(x) {
+  return x * x;
+}
+
+function menyapa(nama) {
+  return "Selamat datang " + nama + " di Binar";
+}
+
+// console.log("Hasil perkalian: ", perkalian(3, 5));
+// console.log("Hasil perkalian salah: ", perkalian(3, "aku"));
+// console.log("Hasil penjumlahan dan perkalian 2: ", jumlahDanKaliDua(3, 5));
+// console.log("Hasil square: ", square(9));
+console.log(menyapa("Ismail"));
---
+++ chapter3/higherOrderFunction.js
@@ -0,0 +1,11 @@
+const programmingLanguages = ["js", "php", "python"];
+
+// for loop
+// for (var i = 0; i < programmingLanguages.length; i++) {
+//   console.log("Language: " + programmingLanguages[i])
+// }
+
+// function forEach
+programmingLanguages.forEach(language => {
+  console.log("Language: " + language)
+})
