2700df3a213bdd9a952dc3821b45259332be33b7
Raihan Rafid <raihanrfd1@gmail.com>
2024-05-28 20:59:56 +0700

    practice: add latihan3

---
+++ chapter3/practice/latihan3.js
@@ -0,0 +1,23 @@
+const users = [
+  { name: "javid", email: "javid@binar.com", password: "123456" },
+  { name: "ario", email: "ario@binar.com", password: "ario123" },
+  { name: "ismail", email: "ismail@binar.com", password: "ismail123" },
+];
+
+async function updateUserByEmail(user) {
+  // TODO: Buatlah function untuk mengupdate data user berdasarkan email 
+  // Kembalikkan error jika user tidak ada
+}
+
+async function deleteUserByEmail(email) {
+  // TODO: Buatlah function untuk menghapus data user berdasarkan email
+  // Kembalikkan error jika user tidak ada
+}
+
+async function getListUser() {
+  return users;
+}
+
+function main() {
+
+}
