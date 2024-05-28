8afbc699536fc19ba34ce1938cf6e1dfadba57ba
Raihan Rafid <raihanrfd1@gmail.com>
2024-05-28 21:04:02 +0700

    fix: change variable data type

--- chapter3/practice/latihan3.js
+++ chapter3/practice/latihan3.js
@@ -1,4 +1,4 @@
-const users = [
+let users = [
   { name: "javid", email: "javid@binar.com", password: "123456" },
   { name: "ario", email: "ario@binar.com", password: "ario123" },
   { name: "ismail", email: "ismail@binar.com", password: "ismail123" },
@@ -7,11 +7,15 @@ const users = [
 async function updateUserByEmail(user) {
   // TODO: Buatlah function untuk mengupdate data user berdasarkan email 
   // Kembalikkan error jika user tidak ada
+  // !Clue: bisa buat array baru yang sesuai dengan input parameter, 
+  // lalu value dari array baru dimasukkan ke array lama
 }
 
 async function deleteUserByEmail(email) {
   // TODO: Buatlah function untuk menghapus data user berdasarkan email
   // Kembalikkan error jika user tidak ada
+  // !Clue: bisa buat array baru yang sesuai dengan input parameter, 
+  // lalu value dari array baru dimasukkan ke array lama
 }
 
 async function getListUser() {
